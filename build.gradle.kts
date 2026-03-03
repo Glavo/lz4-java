91a395bf32451994452f4ec88fa56e9238313ae9
Glavo <zjx001202@gmail.com>
2026-03-04 02:25:16 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "org.glavo"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("com.carrotsearch.randomizedtesting:randomizedtesting-runner:2.7.7")
+    testImplementation("com.code-intelligence:jazzer-junit:0.26.0")
+}
+
+tasks.compileJava {
+    options.release.set(8)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
