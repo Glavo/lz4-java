80c20cdfc27974380ec3b491054c73cf041bfb7b
Glavo <zjx001202@gmail.com>
2026-03-04 02:32:18 +0800

    Convert JavaDoc to Markdown format

--- src/main/java/net/jpountz/lz4/LZ4Factory.java
+++ src/main/java/net/jpountz/lz4/LZ4Factory.java
@@ -39,123 +39,109 @@ public final class LZ4Factory {
 
     private static final LZ4Factory INSTANCE = new LZ4Factory("JavaSafe");
 
-    /**
-     * Returns a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that are native bindings to the original C library.
-     * <p>
-     * Please note that this instance has some traps you should be aware of:<ol>
-     * <li>Upon loading this instance, files will be written to the temporary
-     * directory of the system. Although these files are supposed to be deleted
-     * when the JVM exits, they might remain on systems that don't support
-     * removal of files being used such as Windows.
-     * <li>The instance can only be loaded once per JVM. This can be a problem
-     * if your application uses multiple class loaders (such as most servlet
-     * containers): this instance will only be available to the children of the
-     * class loader which has loaded it. As a consequence, it is advised to
-     * either not use this instance in webapps or to put this library in the lib
-     * directory of your servlet container so that it is loaded by the system
-     * class loader.
-     * <li>From lz4-java version 1.6.0, a {@link LZ4FastDecompressor} instance
-     * returned by {@link #fastDecompressor()} of this instance is SLOWER
-     * than a {@link LZ4SafeDecompressor} instance returned by
-     * {@link #safeDecompressor()}, due to a change in the original LZ4
-     * C implementation. The corresponding C API function is deprecated.
-     * Hence use of {@link #fastDecompressor()} is deprecated
-     * for this instance.
-     * </ol>
-     *
-     * @return a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that are native bindings to the original C library
-     */
+    /// Returns a [LZ4Factory] instance that returns compressors and
+    /// decompressors that are native bindings to the original C library.
+    ///
+    /// Please note that this instance has some traps you should be aware of:<ol>
+    ///   - Upon loading this instance, files will be written to the temporary
+    ///     directory of the system. Although these files are supposed to be deleted
+    ///     when the JVM exits, they might remain on systems that don't support
+    ///     removal of files being used such as Windows.
+    ///   - The instance can only be loaded once per JVM. This can be a problem
+    ///     if your application uses multiple class loaders (such as most servlet
+    ///     containers): this instance will only be available to the children of the
+    ///     class loader which has loaded it. As a consequence, it is advised to
+    ///     either not use this instance in webapps or to put this library in the lib
+    ///     directory of your servlet container so that it is loaded by the system
+    ///     class loader.
+    ///   - From lz4-java version 1.6.0, a [LZ4FastDecompressor] instance
+    ///     returned by [#fastDecompressor()] of this instance is SLOWER
+    ///     than a [LZ4SafeDecompressor] instance returned by
+    ///     [#safeDecompressor()], due to a change in the original LZ4
+    ///     C implementation. The corresponding C API function is deprecated.
+    ///     Hence use of [#fastDecompressor()] is deprecated
+    ///     for this instance.
+    ///     </ol>
+    ///
+    ///     @return a [LZ4Factory] instance that returns compressors and
+    ///     decompressors that are native bindings to the original C library
     public static LZ4Factory nativeInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Insecure variant of {@link #nativeInstance()}. The JNI-based {@link LZ4FastDecompressor} is not secure for
-     * untrusted inputs, so {@link #nativeInstance()} will instead return the slower safe java implementation from
-     * {@link #fastDecompressor()}. If that implementation is too slow for you, it is recommended to move to
-     * {@link #safeDecompressor()}, which is actually faster even than the JNI {@link #fastDecompressor()}. Only if that
-     * is not an option for you, and you can guarantee no untrusted inputs will be decompressed, should you use this
-     * method.
-     *
-     * @return An insecure, JNI-backed LZ4Factory
-     * @deprecated Never decompress untrusted inputs with this instance. Prefer {@link #nativeInstance()}.
-     */
+    /// Insecure variant of [#nativeInstance()]. The JNI-based [LZ4FastDecompressor] is not secure for
+    /// untrusted inputs, so [#nativeInstance()] will instead return the slower safe java implementation from
+    /// [#fastDecompressor()]. If that implementation is too slow for you, it is recommended to move to
+    /// [#safeDecompressor()], which is actually faster even than the JNI [#fastDecompressor()]. Only if that
+    /// is not an option for you, and you can guarantee no untrusted inputs will be decompressed, should you use this
+    /// method.
+    ///
+    /// @return An insecure, JNI-backed LZ4Factory
+    /// @deprecated Never decompress untrusted inputs with this instance. Prefer [#nativeInstance()].
     @Deprecated
     public static LZ4Factory nativeInsecureInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Returns a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that are written with Java's official API.
-     *
-     * @return a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that are written with Java's official API.
-     */
+    /// Returns a [LZ4Factory] instance that returns compressors and
+    /// decompressors that are written with Java's official API.
+    ///
+    /// @return a [LZ4Factory] instance that returns compressors and
+    /// decompressors that are written with Java's official API.
     public static LZ4Factory safeInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Returns a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that may use {@link sun.misc.Unsafe} to speed up compression
-     * and decompression.
-     *
-     * @return a {@link LZ4Factory} instance that returns compressors and
-     * decompressors that may use {@link sun.misc.Unsafe} to speed up compression
-     * and decompression.
-     * @deprecated Note: It is not yet clear which Unsafe-based implementations are secure. Out of caution, this method
-     * currently returns the {@link #safeInstance()}. In a future version, when security has been assessed, this method
-     * may return to Unsafe.
-     */
+    /// Returns a [LZ4Factory] instance that returns compressors and
+    /// decompressors that may use [sun.misc.Unsafe] to speed up compression
+    /// and decompression.
+    ///
+    /// @return a [LZ4Factory] instance that returns compressors and
+    /// decompressors that may use [sun.misc.Unsafe] to speed up compression
+    /// and decompression.
+    /// @deprecated Note: It is not yet clear which Unsafe-based implementations are secure. Out of caution, this method
+    /// currently returns the [#safeInstance()]. In a future version, when security has been assessed, this method
+    /// may return to Unsafe.
     @Deprecated
     public static LZ4Factory unsafeInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Insecure variant of {@link #unsafeInstance()}. The Unsafe-based {@link LZ4FastDecompressor} is not secure for
-     * untrusted inputs, so {@link #unsafeInstance()} will instead return the slower safe java implementation from
-     * {@link #fastDecompressor()}. If that implementation is too slow for you, it is recommended to move to
-     * {@link #safeDecompressor()}. Only if that is not an option for you, and you can guarantee no untrusted inputs will
-     * be decompressed, should you use this method.
-     *
-     * @return An insecure, Unsafe-backed LZ4Factory
-     * @deprecated Never decompress untrusted inputs with this instance. Prefer {@link #unsafeInstance()}.
-     */
+    /// Insecure variant of [#unsafeInstance()]. The Unsafe-based [LZ4FastDecompressor] is not secure for
+    /// untrusted inputs, so [#unsafeInstance()] will instead return the slower safe java implementation from
+    /// [#fastDecompressor()]. If that implementation is too slow for you, it is recommended to move to
+    /// [#safeDecompressor()]. Only if that is not an option for you, and you can guarantee no untrusted inputs will
+    /// be decompressed, should you use this method.
+    ///
+    /// @return An insecure, Unsafe-backed LZ4Factory
+    /// @deprecated Never decompress untrusted inputs with this instance. Prefer [#unsafeInstance()].
     @Deprecated
     public static LZ4Factory unsafeInsecureInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Returns the fastest available {@link LZ4Factory} instance which does not
-     * rely on JNI bindings. It first tries to load the
-     * {@link #unsafeInstance() unsafe instance}, and then the
-     * {@link #safeInstance() safe Java instance} if the JVM doesn't have a
-     * working {@link sun.misc.Unsafe}.
-     *
-     * @return the fastest available {@link LZ4Factory} instance which does not
-     * rely on JNI bindings.
-     */
+    /// Returns the fastest available [LZ4Factory] instance which does not
+    /// rely on JNI bindings. It first tries to load the
+    /// [unsafe instance][#unsafeInstance()], and then the
+    /// [safe Java instance][#safeInstance()] if the JVM doesn't have a
+    /// working [sun.misc.Unsafe].
+    ///
+    /// @return the fastest available [LZ4Factory] instance which does not
+    /// rely on JNI bindings.
     public static LZ4Factory fastestJavaInstance() {
         return INSTANCE;
     }
 
-    /**
-     * Returns the fastest available {@link LZ4Factory} instance. If the class
-     * loader is the system class loader and if the
-     * {@link #nativeInstance() native instance} loads successfully, then the
-     * {@link #nativeInstance() native instance} is returned, otherwise the
-     * {@link #fastestJavaInstance() fastest Java instance} is returned.
-     * <p>
-     * Please read {@link #nativeInstance() javadocs of nativeInstance()} before
-     * using this method.
-     *
-     * @return the fastest available {@link LZ4Factory} instance
-     */
+    /// Returns the fastest available [LZ4Factory] instance. If the class
+    /// loader is the system class loader and if the
+    /// [native instance][#nativeInstance()] loads successfully, then the
+    /// [native instance][#nativeInstance()] is returned, otherwise the
+    /// [fastest Java instance][#fastestJavaInstance()] is returned.
+    ///
+    /// Please read [javadocs of nativeInstance()][#nativeInstance()] before
+    /// using this method.
+    ///
+    /// @return the fastest available [LZ4Factory] instance
     public static LZ4Factory fastestInstance() {
         return INSTANCE;
     }
@@ -199,45 +185,40 @@ public final class LZ4Factory {
 
     }
 
-    /**
-     * Returns a blazing fast {@link LZ4Compressor}.
-     *
-     * @return a blazing fast {@link LZ4Compressor}
-     */
+    /// Returns a blazing fast [LZ4Compressor].
+    ///
+    /// @return a blazing fast [LZ4Compressor]
     public LZ4Compressor fastCompressor() {
         return fastCompressor;
     }
 
-    /**
-     * Returns a {@link LZ4Compressor} which requires more memory than
-     * {@link #fastCompressor()} and is slower but compresses more efficiently.
-     *
-     * @return a {@link LZ4Compressor} which requires more memory than
-     * {@link #fastCompressor()} and is slower but compresses more efficiently.
-     */
+    /// Returns a [LZ4Compressor] which requires more memory than
+    /// [#fastCompressor()] and is slower but compresses more efficiently.
+    ///
+    /// @return a [LZ4Compressor] which requires more memory than
+    /// [#fastCompressor()] and is slower but compresses more efficiently.
     public LZ4Compressor highCompressor() {
         return highCompressor;
     }
 
-    /**
-     * Returns a {@link LZ4Compressor} which requires more memory than
-     * {@link #fastCompressor()} and is slower but compresses more efficiently.
-     * The compression level can be customized.
-     * <p>For current implementations, the following is true about compression level:<ol>
-     * <li>It should be in range [1, 17]</li>
-     * <li>A compression level higher than 17 would be treated as 17.</li>
-     * <li>A compression level lower than 1 would be treated as 9.</li>
-     * </ol>
-     * Note that compression levels from different implementations
-     * (native, unsafe Java, and safe Java) cannot be compared with one another.
-     * Specifically, the native implementation of a high compression level
-     * is not necessarily faster than the safe/unsafe Java implementation
-     * of the same compression level.
-     *
-     * @param compressionLevel the compression level between [1, 17]; the higher the level, the higher the compression ratio
-     * @return a {@link LZ4Compressor} which requires more memory than
-     * {@link #fastCompressor()} and is slower but compresses more efficiently.
-     */
+    /// Returns a [LZ4Compressor] which requires more memory than
+    /// [#fastCompressor()] and is slower but compresses more efficiently.
+    /// The compression level can be customized.
+    ///
+    /// For current implementations, the following is true about compression level:<ol>
+    ///   - It should be in range [1, 17]
+    ///   - A compression level higher than 17 would be treated as 17.
+    ///   - A compression level lower than 1 would be treated as 9.
+    /// </ol>
+    /// Note that compression levels from different implementations
+    /// (native, unsafe Java, and safe Java) cannot be compared with one another.
+    /// Specifically, the native implementation of a high compression level
+    /// is not necessarily faster than the safe/unsafe Java implementation
+    /// of the same compression level.
+    ///
+    /// @param compressionLevel the compression level between [1, 17]; the higher the level, the higher the compression ratio
+    /// @return a [LZ4Compressor] which requires more memory than
+    /// [#fastCompressor()] and is slower but compresses more efficiently.
     public LZ4Compressor highCompressor(int compressionLevel) {
         if (compressionLevel > MAX_COMPRESSION_LEVEL) {
             compressionLevel = MAX_COMPRESSION_LEVEL;
@@ -247,51 +228,41 @@ public final class LZ4Factory {
         return highCompressors[compressionLevel];
     }
 
-    /**
-     * Returns a {@link LZ4FastDecompressor} instance.
-     * Use of this method is deprecated for the {@link #nativeInstance() native instance}.
-     *
-     * @return a {@link LZ4FastDecompressor} instance
-     * @see #nativeInstance()
-     */
+    /// Returns a [LZ4FastDecompressor] instance.
+    /// Use of this method is deprecated for the [native instance][#nativeInstance()].
+    ///
+    /// @return a [LZ4FastDecompressor] instance
+    /// @see #nativeInstance()
     public LZ4FastDecompressor fastDecompressor() {
         return fastDecompressor;
     }
 
-    /**
-     * Returns a {@link LZ4SafeDecompressor} instance.
-     *
-     * @return a {@link LZ4SafeDecompressor} instance
-     */
+    /// Returns a [LZ4SafeDecompressor] instance.
+    ///
+    /// @return a [LZ4SafeDecompressor] instance
     public LZ4SafeDecompressor safeDecompressor() {
         return safeDecompressor;
     }
 
-    /**
-     * Returns a {@link LZ4UnknownSizeDecompressor} instance.
-     *
-     * @return a {@link LZ4UnknownSizeDecompressor} instance
-     * @deprecated use {@link #safeDecompressor()}
-     */
+    /// Returns a [LZ4UnknownSizeDecompressor] instance.
+    ///
+    /// @return a [LZ4UnknownSizeDecompressor] instance
+    /// @deprecated use [#safeDecompressor()]
     public LZ4UnknownSizeDecompressor unknownSizeDecompressor() {
         return safeDecompressor();
     }
 
-    /**
-     * Returns a {@link LZ4Decompressor} instance.
-     *
-     * @return a {@link LZ4Decompressor} instance
-     * @deprecated use {@link #fastDecompressor()}
-     */
+    /// Returns a [LZ4Decompressor] instance.
+    ///
+    /// @return a [LZ4Decompressor] instance
+    /// @deprecated use [#fastDecompressor()]
     public LZ4Decompressor decompressor() {
         return fastDecompressor();
     }
 
-    /**
-     * Prints the fastest instance.
-     *
-     * @param args no argument required
-     */
+    /// Prints the fastest instance.
+    ///
+    /// @param args no argument required
     public static void main(String[] args) {
         System.out.println("Fastest instance is " + fastestInstance());
         System.out.println("Fastest Java instance is " + fastestJavaInstance());
