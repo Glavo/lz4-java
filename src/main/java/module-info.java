3f117e655bfb48b80328dfb78d4018e73ef31982
Glavo <zjx001202@gmail.com>
2026-03-04 11:38:18 +0800

    Suppress module warning in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@
 /// **LZ4 - Java** is a Java port of the popular
 /// <a href="https://github.com/lz4/lz4">lz4 compression algorithms</a> and
 /// <a href="https://github.com/Cyan4973/xxHash">xxHash hashing algorithm</a>.
+@SuppressWarnings("module")
 module org.lz4.java {
     exports net.jpountz.lz4;
     exports net.jpountz.xxhash;
