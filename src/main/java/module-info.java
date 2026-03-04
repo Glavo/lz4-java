dd2ced1a448641118898c85ef8427c3fd446a6a1
Glavo <zjx001202@gmail.com>
2026-03-04 09:20:17 +0800

    Add module-info.java for Java module system

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+///
+/// **LZ4 - Java** is a Java port of the popular
+/// <a href="https://github.com/lz4/lz4">lz4 compression algorithms</a> and
+/// <a href="https://github.com/Cyan4973/xxHash">xxHash hashing algorithm</a>.
+module org.lz4.java {
+    exports net.jpountz.lz4;
+    exports net.jpountz.xxhash;
+}
